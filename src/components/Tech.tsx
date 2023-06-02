804618a17f019ffdcf5231c78d50a1811429d310
alejandro <alejandrober96@gmail.com>
2023-06-02 10:12:38 -0500

    Finish component calling TechCard component

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,16 +1,24 @@
+import TechCard from "./TechCard";
+
 function Tech() {
 
   const techObject = {
-      Frameworks: ['React','Next js', 'Ruby on Rails'],
-      Languages: ['TypeScript', 'JavaScript', 'Ruby', 'HTML', 'CSS'],
-      Databases: ['PostgreSQL'],
-      Tools: ['Git', 'GitHub', 'VS code', 'Postman', 'Figma', 'Vercel']
+      'Frameworks': ['React','Next js', 'Ruby on Rails'],
+      'Languages': ['TypeScript', 'JavaScript', 'Ruby', 'HTML', 'CSS'],
+      'Databases': ['PostgreSQL'],
+      'Tools': ['Git', 'GitHub', 'VS code', 'Postman', 'Figma', 'Vercel']
     }
-
-  return (
-    <div>
-      <h1>Tech</h1>
-    </div>
+    return (
+      <section>
+      <h2>Tech Stack</h2>
+      <div className="tech-container">
+        {Object.keys(techObject).map((tech) => {
+          return (
+            <TechCard key={tech} title={tech} stack={techObject[tech]}/>
+          )
+        })}
+      </div>
+    </section>
   );
 }
 export default Tech;
