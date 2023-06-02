af413757e09ff678a43de15122d4534a8cee1993
alejandro <alejandrober96@gmail.com>
2023-06-02 09:33:23 -0500

    Add object with the content of the cards

---
+++ src/components/Tech.tsx
@@ -0,0 +1,16 @@
+function Tech() {
+
+  const techObject = {
+      Frameworks: ['React','Next js', 'Ruby on Rails'],
+      Languages: ['TypeScript', 'JavaScript', 'Ruby', 'HTML', 'CSS'],
+      Databases: ['PostgreSQL'],
+      Tools: ['Git', 'GitHub', 'VS code', 'Postman', 'Figma', 'Vercel']
+    }
+
+  return (
+    <div>
+      <h1>Tech</h1>
+    </div>
+  );
+}
+export default Tech;
