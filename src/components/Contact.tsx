9c9187ea0d142e7a0f075953f9fb6ef2dc386551
alejandro <alejandrober96@gmail.com>
2023-07-07 12:09:22 -0500

    Change background and add project img

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Contact.module.css';
 function Contact() {
   return (
     <section className={styles.section}>
-      <h2>Contact me.</h2>
+      <h2>Contact me!</h2>
       <div className={styles.formContainer}>
         <p> I would love to build amazing projects together, let&apos;s get in touch.</p>
        <Form />
