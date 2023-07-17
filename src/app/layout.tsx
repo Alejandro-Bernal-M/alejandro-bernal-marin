e3c9e48456216ce933b93a5136943f820193db18
Alejandro Bernal Marín <alejandrober96@gmail.com>
2023-07-17 16:51:09 -0500

    Revert "Revert "Navbar and footer""

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import fondo from '../resources/images/background4.svg'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,7 +25,9 @@ export default function RootLayout({
         backgroundRepeat: 'repeat',
       }}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Logo from '../resources/images/abm_logo2.png'
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
+import { useWindowDimentions } from '../../utils/useWindowDimentionsHook'
+import { Divide as Hamburger } from 'hamburger-react'
+import { useState } from 'react'
+import Popup from './Popup'
+
+const Navbar = () => {
+  const { width } = useWindowDimentions()
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <Image src={Logo} alt='Logo' className={styles.logo}/>
+        {width > 768 && (
+          <ul className={styles.navbarList}>
+            <li className={styles.navbarItem}>
+              <a href='#home-section' className={styles.navbarLink}>Home</a>
+            </li>
+            <li className={styles.navbarItem}>
+              <a href='#about-section' className={styles.navbarLink}>About</a>
+            </li>
+            <li className={styles.navbarItem}>
+              <a href='#projects-section' className={styles.navbarLink}>Projects</a>
+            </li>
+            <li className={styles.navbarItem}>
+              <a href='#contact-section' className={styles.navbarLink}>Contact</a>
+            </li>
+          </ul>
+        )}
+        {width <= 768 && (
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            onToggle={(toggled) => {
+              let popup = document.querySelector('#popup');
+              if (toggled) {
+                popup?.classList.add(styles.popupActive)
+              } else {
+                popup?.classList.remove(styles.popupActive)
+              }
+            }}
+          />
+          )}
+      </nav>
+      {width <= 768 && <Popup />}
+    </header>
+  )
+}
+
+export default Navbar
